ca39430e4af7591e74eef732dfa0b7b88c53deb5
Bliclk <iperez2023@udec.cl>
2024-07-10 00:01:07 -0200

    rediseño del sistema de creacion y eliminacion de notificaciones

--- src/main/java/GUI/PanelNotificaciones.java
+++ src/main/java/GUI/PanelNotificaciones.java
@@ -8,11 +8,16 @@ import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class PanelNotificaciones extends JPanel {
     private Font fuente;
     private ArrayList<Habitat> habitats;
-    private JTextField notificacion;
+    private HashMap<Animal,ArrayList<JTextField>> fieldsAnimales = new HashMap<>();
+    private HashMap<Habitat,ArrayList<JTextField>> fieldsHabitats = new HashMap<>();
+    private HashMap<JTextField, Integer> notificaciones = new HashMap<>();
+    private ArrayList<Animal> animales;
+    private int ticksDeNotiTemporales = 600;
 
     public PanelNotificaciones(ArrayList habitats){
         try {
@@ -27,20 +32,67 @@ public class PanelNotificaciones extends JPanel {
         setBounds(17,514,464,350);
     }
 
-    public void createNotificacion(){
-        for (Habitat i : habitats){
-            for (Animal j : i.getAnimals()){
-                if (j.getFelicidad() <= 10){
-                    notificacion = new JTextField("Un " + j.getNombre() + " esta muy triste por las condiciones en las que vive");
-                    configurar(notificacion);
-                    add(notificacion);
+    public void createNotificacion() {
+        animales = new ArrayList<>();
+
+        for (Habitat i : habitats) {
+            animales.addAll(i.getAnimals());
+        }
+
+        if (animales.size() != fieldsAnimales.size()) {
+            for (Animal i : animales) {
+                if (!fieldsAnimales.containsKey(i)) {
+                    ArrayList<JTextField> textFields = new ArrayList<>();
+                    textFields.add(new JTextField("Un/a " + i.getNombre() + " esta muy triste por las condiciones en las que vive"));
+                    textFields.add(new JTextField("Se han llevado a un/a " + i.getNombre() + "debido a su infelicidad"));
+                    fieldsAnimales.put(i, (textFields));
                 }
             }
-            if (i.getComidaHierba() == 0 || i.getComidaCarne() == 0){
-                notificacion = new JTextField("Se acabo la comida en un " + i.getTipoDeHabitat().getNombre());
-                configurar(notificacion);
-                add(notificacion);
+            fieldsAnimales.keySet().retainAll(animales);
+        }
+
+        if (habitats.size() != fieldsHabitats.size()){
+            for (Habitat i : habitats){
+                if (!fieldsHabitats.containsKey(i)) {
+                    ArrayList<JTextField> textFields = new ArrayList<>();
+                    textFields.add(new JTextField("Se acabo la comida en un " + i.getTipoDeHabitat().getNombre()));
+                    fieldsHabitats.put(i, (textFields));
+                }
             }
+            fieldsHabitats.keySet().retainAll(habitats);
+        }
+
+        if (!fieldsAnimales.isEmpty()) {
+            fieldsAnimales.forEach((k, t) -> {
+                System.out.println();
+                if (k.getFelicidad() <= 10) {
+                    configurar(t.get(0));
+                    add(t.get(0));
+                }else{
+                    remove(t.get(0));
+                }
+
+                if (k.getInfelicidad() >= 10) {
+                    configurar(t.get(1));
+                    add(t.get(1));
+                    remove(t.get(0));
+                    k.getHabitat().getAnimals().remove(k);
+                    notificaciones.put(t.get(1),0);
+                }else if (k.getInfelicidad() == 0){
+                    remove(t.get(1));
+                }
+            });
+        }
+
+        if (!fieldsHabitats.isEmpty()) {
+            fieldsHabitats.forEach((k, t) -> {
+                if (k.getComidaHierba() == 0 || k.getComidaCarne() == 0) {
+                    configurar(t.get(0));
+                    add(t.get(0));
+                } else {
+                    remove(t.get(0));
+                }
+            });
         }
     }
 
@@ -51,5 +103,30 @@ public class PanelNotificaciones extends JPanel {
         j.setPreferredSize(new Dimension(464,20));
         j.setForeground(new Color(255,245,213));
         j.setOpaque(false);
+    }
+
+    private void quitarNotificaciones(){
+        ArrayList<JTextField> notificacionesTemporales = new ArrayList<>(notificaciones.keySet());
+        if (!notificaciones.isEmpty()) {
+            notificaciones.forEach((n, x) -> {
+                x++;
+                notificaciones.replace(n, x);
+                if (n == null){
+                    notificacionesTemporales.remove(n);
+                }
+                if (x > ticksDeNotiTemporales) {
+                    remove(n);
+                    System.out.println(x);
+                    notificacionesTemporales.remove(n);
+                }
+            });
+            notificaciones.keySet().retainAll(notificacionesTemporales);
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        quitarNotificaciones();
     }
 }
